5bec15d75eef0959891ea122fb5e6086212d5fa9
Ayc0 <ayc0.benj@gmail.com>
2019-03-06 17:19:09 +0100

    [react-manatea] refactor infuser and infuse with useInfuser

--- packages/react-manatea/src/Infuser.tsx
+++ packages/react-manatea/src/Infuser.tsx
@@ -1,29 +1,17 @@
-import * as React from 'react';
-import { Cup, Tea, Listener } from 'manatea';
+import { ReactNode } from 'react';
+import { Cup, Tea } from 'manatea';
 
 import getCup from './getCup';
+import useInfuser from './useInfuser';
 
 interface Props {
   cup: Cup;
-  children: (tea: Tea) => React.ReactNode;
+  children: (tea: Tea) => ReactNode;
 }
 
-interface State {
-  tea: Tea;
-}
+const Infuser = (props: Props) => {
+  const [tea] = useInfuser(getCup(props.cup));
+  return props.children(tea);
+};
 
-export default class Infuser extends React.Component<Props, State> {
-  state: State = { tea: getCup(this.props.cup)() };
-  listener: Listener = getCup(this.props.cup).on((tea: Tea) =>
-    this.setState({ tea }),
-  );
-
-  componentWillUnmount() {
-    if (this.listener.listening) {
-      this.listener();
-    }
-  }
-  render() {
-    return this.props.children(this.state.tea);
-  }
-}
+export default Infuser;
--- packages/react-manatea/src/infuse.tsx
+++ packages/react-manatea/src/infuse.tsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
-import { Cup, Tea, Listener } from 'manatea';
+import { Cup } from 'manatea';
 
 import getCup from './getCup';
+import useInfuser from './useInfuser';
 
-interface State {
-  tea: Tea;
-}
+export default (cup: Cup) => (component: React.ComponentType<any>) => {
+  const Consumer = (props: any) => {
+    const [tea] = useInfuser(getCup(cup));
 
-export default (cup: Cup) => (component: React.ComponentType<any>) =>
-  class Consumer extends React.Component<any, State> {
-    state: State = { tea: getCup(cup)() };
-    listener: Listener = getCup(cup).on((tea: Tea) => this.setState({ tea }));
-    componentWillUnmount() {
-      if (this.listener.listening) {
-        this.listener();
-      }
-    }
-    render() {
-      return React.createElement(component, {
-        ...this.props,
-        tea: this.state.tea,
-      });
-    }
+    return React.createElement(component, {
+      ...props,
+      tea,
+    });
   };
+  return Consumer;
+};

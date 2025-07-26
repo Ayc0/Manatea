ccf39e92fb7d64c0eb8ce4b96e5990d26be7725c
Ayc0 <ayc0.benj@gmail.com>
2025-07-26 10:06:37 +0200

    switch to pnpm

--- deploy.js
+++ deploy.js
@@ -22,22 +22,18 @@ addTag(version);
 push();
 
 function* listAllWorkspaces() {
-  const output = cp.spawnSync('yarn', ['workspaces', 'list', '--json'], {
+  const output = cp.spawnSync('pnpm', ['-r', 'exec', 'pwd'], {
     encoding: 'utf-8',
   }).stdout;
-  const workspaces = output.trim().split('\n').map(JSON.parse);
+  const paths = output.trim().split('\n');
 
-  for (const workspace of workspaces) {
-    const packageJsonPath = path.join(
-      __dirname,
-      workspace.location,
-      'package.json',
-    );
+  for (const p of paths) {
+    const packageJsonPath = path.join(p, 'package.json');
     const packageJson = JSON.parse(
       fs.readFileSync(packageJsonPath, { encoding: 'utf-8' }),
     );
 
-    yield { packageJsonPath, packageJson, name: workspace.name };
+    yield { packageJsonPath, packageJson, name: packageJson.name };
   }
 }
 
@@ -52,32 +48,22 @@ function updateAllVersions(version) {
 }
 
 function deploy(package) {
-  cp.spawnSync('yarn', ['npm', 'publish', ...argv], {
+  cp.spawnSync('pnpm', ['publish', ...argv], {
     cwd: path.join(__dirname, 'packages', package),
     stdio: 'inherit',
   });
 }
 
 function addTag(version) {
-  cp.spawnSync('git', ['tag', `v${version}`], {
-    stdio: 'inherit',
-  });
+  cp.spawnSync('git', ['tag', `v${version}`], { stdio: 'inherit' });
 }
 
 function commit(version) {
-  cp.spawnSync('git', ['add', '.'], {
-    stdio: 'inherit',
-  });
-  cp.spawnSync('git', ['commit', '-m', `v${version}`], {
-    stdio: 'inherit',
-  });
+  cp.spawnSync('git', ['add', '.'], { stdio: 'inherit' });
+  cp.spawnSync('git', ['commit', '-m', `v${version}`], { stdio: 'inherit' });
 }
 
 function push() {
-  cp.spawnSync('git', ['push'], {
-    stdio: 'inherit',
-  });
-  cp.spawnSync('git', ['push', '--tags'], {
-    stdio: 'inherit',
-  });
+  cp.spawnSync('git', ['push'], { stdio: 'inherit' });
+  cp.spawnSync('git', ['push', '--tags'], { stdio: 'inherit' });
 }
